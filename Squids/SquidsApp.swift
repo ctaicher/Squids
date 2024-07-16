e22a0e0734b591d9662a5a9dbf1e3cb649360b8b
ctaicher <cltaicher@gmail.com>
2024-07-16 16:37:54 -0700

    Initial Commit

---
+++ Squids/SquidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquidsApp.swift
+//  Squids
+//
+//  Created by Christopher Taicher on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SquidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
